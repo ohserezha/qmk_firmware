f4b2d7567476f02fed7315efc604c03cd2198921
Serhii <gssreg@gmail.com>
2025-01-09 11:49:22 -0500

    make complete layout test

--- keyboards/boardsource/unicorne/keymaps/achordion/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/achordion/keymap.c
@@ -8,9 +8,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC, 
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, 
+        KC_TAB,  KC_A, LCTL_T(KC_S), LALT_T(KC_D),   LGUI_T(KC_F),    KC_G,    KC_H,    RGUI_T(KC_J),    RALT_T(KC_K),    RCTL_T(KC_L),   RSFT_T(KC_SCLN), KC_QUOT, 
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ESC, 
-                                   KC_LGUI, MO(1),   KC_SPC,  KC_ENT,  MO(2),   KC_RALT
+                                   KC_LGUI, MO(1),   LSFT_T(KC_ENT),   KC_SPC,  LT(2, KC_BSPC),   KC_RALT
         ),
     [1] = LAYOUT_split_3x6_3(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, 
