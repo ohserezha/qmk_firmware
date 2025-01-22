6067b8017c622fe5e611682fed348e8dfd9ffa88
Serhii <gssreg@gmail.com>
2025-01-21 20:42:40 -0500

    settled keymap

--- keyboards/boardsource/unicorne/keymaps/achordion/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/achordion/keymap.c
@@ -5,26 +5,54 @@
 
 #include "features/achordion.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC, 
-        KC_TAB,  KC_A, LCTL_T(KC_S), LALT_T(KC_D),   LGUI_T(KC_F),    KC_G,    KC_H,    RGUI_T(KC_J),    RALT_T(KC_K),    RCTL_T(KC_L),   RSFT_T(KC_SCLN), KC_QUOT, 
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ESC, 
-                                   KC_LGUI, MO(1),   LSFT_T(KC_ENT),   KC_SPC,  LT(2, KC_BSPC),   KC_RALT
-        ),
-    [1] = LAYOUT_split_3x6_3(
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, 
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, 
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-                                   _______, _______, _______, _______, _______, _______
-        ),
-    [2] = LAYOUT_split_3x6_3(
-        QK_BOOT, _______, _______, _______, _______, _______, RM_VALU, RM_HUEU, RM_SATU, RM_NEXT, RM_TOGG, _______, 
-        EE_CLR,  _______, _______, _______, _______, _______, RM_VALD, RM_HUED, RM_SATD, RM_PREV, CK_TOGG, _______, 
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-                                   _______, _______, _______, _______, _______, _______
-        )
+enum layer_names {
+    _BAS,
+    _NUM,
+    _NAV,
+    _MED,
+    _BRC,
+    _SYM
 };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BAS] = LAYOUT_split_3x6_3(
+      KC_ESC,       KC_Q,         KC_W,         KC_E,             KC_R,             KC_T,       KC_Y,     KC_U,             KC_I,         KC_O,         KC_P,            KC_LBRC, 
+      KC_TAB,       LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D),     LGUI_T(KC_F),     KC_G,       KC_H,     RGUI_T(KC_J),     RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT, 
+      LCA(KC_SPC),  KC_Z,         KC_X,         KC_C,             KC_V,             KC_B,       KC_N,     KC_M,             KC_COMM,      KC_DOT,       KC_SLSH,         KC_RBRC, 
+                                                LT(_NUM,KC_CAPS), LT(_NAV,KC_SPC),  MO(_BRC),   KC_ENT,   LT(_SYM,KC_BSPC), MO(_MED)
+      ),
+    [_NUM] = LAYOUT_split_3x6_3(
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_7,     KC_8,     KC_9,   KC_NO,  KC_NO, 
+      KC_NO,    KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_4,     KC_5,     KC_6,   KC_0,   KC_NO, 
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_1,     KC_2,     KC_3,   KC_NO,  KC_NO, 
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+      ),
+    [_NAV] = LAYOUT_split_3x6_3(
+      KC_TRNS, KC_TRNS,     KC_TRNS,    SGUI(KC_LBRC),  SGUI(KC_RBRC),  KC_TRNS,  KC_TRNS,  RGUI(KC_LEFT),  KC_UP,    RGUI(KC_RGHT),  KC_TRNS,  KC_TRNS, 
+      KC_TRNS, KC_LSFT,     KC_LCTL,    KC_LALT,        KC_LGUI,        KC_TRNS,  KC_TRNS,  KC_LEFT,        KC_DOWN,  KC_RGHT,        KC_TRNS,  KC_TRNS, 
+      KC_TRNS, LGUI(KC_Z),  LGUI(KC_X), LGUI(KC_C),     LGUI(KC_V),     KC_TRNS,  KC_TRNS,  RALT(KC_LEFT),  KC_TRNS,  RALT(KC_RGHT),  KC_TRNS,  KC_TRNS, 
+                                        KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS
+      ),
+    [_MED] = LAYOUT_split_3x6_3(
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+      KC_NO, KC_NO, KC_NO, KC_BRID, KC_BRIU, RGB_TOG, KC_NO, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO, 
+      KC_NO, RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, 
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+      ),
+    [_BRC] = LAYOUT_split_3x6_3(
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LPRN, KC_RPRN, KC_AMPR, KC_NO,   KC_NO, 
+      KC_NO,    KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, KC_NO, 
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LBRC, KC_RBRC, KC_QUES, KC_SLSH, KC_NO, 
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+      ),
+    [_SYM] = LAYOUT_split_3x6_3(
+      KC_NO,    KC_GRV,  KC_NO,   KC_HASH, KC_DLR,  KC_PERC, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, 
+      KC_NO,    KC_EXLM, KC_AT,   KC_UNDS, KC_EQL,  KC_CIRC, KC_NO,   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_RSFT,  KC_NO, 
+      KC_NO,    KC_TILD, KC_NO,   KC_MINS, KC_PLUS, KC_ASTR, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, 
+                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS
+      )
+};
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   if (!process_achordion(keycode, record)) { return false; }
@@ -33,8 +61,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   return true;
 }
 
-void matrix_scan_user(void) {
+void housekeeping_task_user(void) {
   achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Exceptionally consider the following chords as holds, even though they
+  // are on the same hand.
+  switch (tap_hold_keycode) {
+    case LT(_NUM,KC_CAPS):
+      return true;
+    case LT(_NAV,KC_SPC): 
+      return true;
+    case LT(_SYM,KC_BSPC):
+      return true;
+    case LGUI_T(KC_F):
+      if (other_keycode == KC_TAB) { return true; }
+      break;
+  }
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
 
